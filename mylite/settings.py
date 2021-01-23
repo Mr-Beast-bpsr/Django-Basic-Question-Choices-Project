d37ef8b06d352a8467b472b8b69b101e2603cdff
Mr-Beast-bpsr <bpsr.rana@gmail.com>
2021-01-23 11:28:18 +0530

    Django-website

--- mylite/settings.py
+++ mylite/settings.py
@@ -32,12 +32,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'mylite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+#
+STATICFILES_DIRS = [
+    (BASE_DIR / "static"),
+]
--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question",{'fields':['question_text']}),
+        ("Date Information", {'fields':['pub_date'],'classes':['collapse']}),
+
+    ]
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+admin.site.register(Question, QuestionAdmin)
+
